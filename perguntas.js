dc7d05290fe6d694264e7935d3aa75c92f5d9429
stefanatojulia <00001105346638sp@al.educacao.sp.gov.br>
2024-11-20 15:05:53 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capotal da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Língua Inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em Inglês é Hi (RAI)'
+)
